a532662a4a112435ae698eba8b12545f6b9b44d8
Bhuvaneshwaran P <logeshbhuvanesh2123@gmail.com>
2024-08-24 20:12:03 +0530

    Fix hight and width of Grid

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ const inputBtn=document.querySelector(".inptbtn");
 
 function createGrid(times) {
     if (times > 100) return alert("Input Exceeds the Maximum Input");
-    container.innerHTML = '';
+    container.innerHTML = '';//Removes Existing Grids in container If there any
     
     for (let i = 0; i < times * times; i++) {
         const grid = document.createElement("div");
@@ -16,7 +16,16 @@ function createGrid(times) {
 
         container.appendChild(grid);
     }
+    
 }
 
-createGrid(16);
+inputBtn.addEventListener("click",() =>{
+    container.innerHTML = ''; //Removes Existing Grids in container
+    let userInput=parseInt(prompt("Enter new grid size (maximum 100):",""));
+    if (userInput && userInput > 0 && userInput <= 100) {
+        createGrid(userInput);
+    } else {
+        alert("Invalid size! Please enter a number between 1 and 50.");
+    }
 
+});
